ba662ee43d0e7fe3d947c039f177b9b7a9169cab
Jotkanwal <31306102+Jotkanwal@users.noreply.github.com>
2020-12-06 23:20:44 -0800

    Add files via upload

--- MininetStarterCode.py
+++ MininetStarterCode.py
@@ -11,17 +11,14 @@ class MyTopo(Topo):
         h1 = self.addHost( 'h1' )
         s1 = self.addSwitch( 's1', listenPort=6635 )
         h2 = self.addHost( 'h2' )
+        s2 = self.addSwitch( 's2', listenPort=6636 )
         h4 = self.addHost( 'h4' )
+        s4 = self.addSwitch( 's4', listenPort=6638 )
         h5 = self.addHost( 'h5' )
         h6 = self.addHost( 'h6' )
         h7 = self.addHost( 'h7' )
         h8 = self.addHost( 'h8' )
-
-        s1 = self.addSwitch( 's1', listenPort=6635 )
-        s2 = self.addSwitch( 's2', listenPort=6636 )
         s3 = self.addSwitch( 's3', listenPort=6637 )
-        s4 = self.addSwitch( 's4', listenPort=6638 )
-
 
         # Adding links between hosts and switches
         self.addLink( h1, s1 )
@@ -67,25 +64,7 @@ def starter():
         h8.intf( 'h8-eth0' ).setIP( '10.0.8.2', 24 )
         h8.intf( 'h8-eth0' ).setMAC( '0A:00:08:02:00:00' )
 
-        s1 = net.get( 's1' )
-        s1.intf( 's1-eth1' ).setMAC( '0A:00:01:01:00:01' )
-        s1.intf( 's1-eth2' ).setMAC( '0A:00:0A:FE:00:02' )
-        s1.intf( 's1-eth3' ).setMAC( '0A:00:0C:01:00:03' )
-        s2 = net.get( 's2' )
-        s2.intf( 's2-eth1' ).setMAC( '0A:00:02:01:00:01' )
-        s2.intf( 's2-eth2' ).setMAC( '0A:00:0B:FE:00:02' )
-        s2.intf( 's2-eth3' ).setMAC( '0A:00:0D:01:00:03' )
-        s2.intf( 's2-eth4' ).setMAC( '0A:00:0C:FE:00:04' )
-        s3 = net.get( 's3' )
-        s3.intf( 's3-eth1' ).setMAC( '0A:00:03:01:00:01' )
-        s3.intf( 's3-eth2' ).setMAC( '0A:00:0D:FE:00:02' )
-        s3.intf( 's3-eth3' ).setMAC( '0A:00:0E:01:00:03' )
-        s3.intf( 's3-eth4' ).setMAC( '0A:00:0F:01:00:04' )
-        s4 = net.get( 's4' )
-        s4.intf( 's4-eth1' ).setMAC( '0A:00:04:01:00:01' )
-        s4.intf( 's4-eth2' ).setMAC( '0A:00:0E:FE:00:02' )
-        s4.intf( 's4-eth3' ).setMAC( '0A:00:10:01:00:03' )
-
+        
         net.start()
         dumpNodeConnections(net.hosts)
         CLI(net)
